ba54a71f644b6486f7ba1ec8da16b0258ef0da9e
Rafael Rodrigues Pereira <rafaelrpdev@gmail.com>
2020-02-02 19:39:57 -0300

    feat: add read .env file

--- getids.py
+++ getids.py
@@ -1,9 +1,11 @@
 #!/usr/local/bin/python3
 from telethon import TelegramClient
+from dotenv import load_dotenv
 import os
 
-api_id = os.environ.get('API_ID')
-api_hash = os.environ.get('API_HASH')
+load_dotenv()
+api_id = os.getenv('API_ID')
+api_hash = os.getenv('API_HASH')
 
 client = TelegramClient('anon', api_id, api_hash)
 
