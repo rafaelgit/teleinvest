3167d75d89be81a5ef76c138ea0e18612c2a6a00
Rafael Rodrigues Pereira <rafaelrpdev@gmail.com>
2020-02-02 20:15:00 -0300

    feat: read multiple receveid ids

--- forward.py
+++ forward.py
@@ -4,16 +4,19 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-api_id = os.environ.get('API_ID')
-api_hash = os.environ.get('API_HASH')
+api_id = os.getenv('API_ID')
+api_hash = os.getenv('API_HASH')
+received_ids = os.getenv('RECEIVED_IDS')
+forward_id = os.getenv('FORWARD_ID')
 
 client = TelegramClient('session_name', api_id, api_hash)
 client.start()
 
 @client.on(events.NewMessage)
 async def main(event):
-    if (event.chat_id == 1):
-        await event.forward_to(2)
+    print(event.chat_id)
+    if (received_ids.find(str(event.chat_id)) >= 0):
+        await event.forward_to(int(forward_id))
 
 with client:
     client.run_until_disconnected()
